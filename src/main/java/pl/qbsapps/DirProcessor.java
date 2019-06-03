3ac551565f95ae8e308132caf1370a170998508a
Damian Kubiak <kubiak.damian96@gmail.com>
2019-06-03 23:13:47 +0200

    Counting signs in a directory added

--- src/main/java/pl/qbsapps/DirProcessor.java
+++ src/main/java/pl/qbsapps/DirProcessor.java
@@ -1,4 +1,32 @@
 package pl.qbsapps;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
 public class DirProcessor {
+    public int countLetters(String path) {
+        FileProcessor fileProcessor = new FileProcessor();
+        ArrayList<String> paths = new ArrayList<>();
+        int sum = 0;
+
+        try (Stream<Path> filePathStream = Files.walk(Paths.get(path))) {
+            filePathStream.forEach(filePath -> {
+                if (Files.isRegularFile(filePath)) {
+                    paths.add(filePath.toString());
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (String filePath : paths) {
+            sum += fileProcessor.countLetters(filePath);
+        }
+
+        return sum;
+    }
 }
--- src/main/java/pl/qbsapps/FileProcessor.java
+++ src/main/java/pl/qbsapps/FileProcessor.java
@@ -9,11 +9,17 @@ public class FileProcessor {
     public int countLetters(String filePath) {
         Path path = Paths.get(filePath);
         String data = null;
+
         try {
             data = new String(Files.readAllBytes(path));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return data.length();
+
+        if (data != null) {
+            return data.length();
+        }
+
+        return 0;
     }
 }
