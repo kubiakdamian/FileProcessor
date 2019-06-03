6314e4df8124387e88302939a46ddbcbe71064dc
Damian Kubiak <kubiak.damian96@gmail.com>
2019-06-03 22:59:20 +0200

    Counting signs in a file added

--- src/main/java/pl/qbsapps/FileProcessor.java
+++ src/main/java/pl/qbsapps/FileProcessor.java
@@ -1,5 +1,19 @@
 package pl.qbsapps;
 
-public class FileProcessor {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+public class FileProcessor {
+    public int countLetters(String filePath) {
+        Path path = Paths.get(filePath);
+        String data = null;
+        try {
+            data = new String(Files.readAllBytes(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return data.length();
+    }
 }
--- src/main/java/pl/qbsapps/Main.java
+++ src/main/java/pl/qbsapps/Main.java
@@ -3,6 +3,6 @@ package pl.qbsapps;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
     }
 }
